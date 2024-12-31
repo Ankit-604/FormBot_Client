308630eb5d8d291e9be11551c8132b9a2fc46f34
Ankit Kumar Sharma <kr704666@gmail.com>
2024-12-31 17:38:25 +0530

    updating

--- src/pages/ThankYou/ThankYou.jsx
+++ src/pages/ThankYou/ThankYou.jsx
@@ -3,15 +3,8 @@ import styles from "./thankyou.module.css";
 const ThankYou = () => {
   return (
     <div className={styles.thankYouContainer}>
-      <div className={styles.overlay}>
-        <h1>Thank You for your response</h1>
-        <p>Your feedback helps us improve and grow. We truly appreciate it!</p>
-        <img
-          className={styles.image}
-          src="https://www.freepik.com/free-vector/thank-you-background-with-lettering-watercolor-stain_2390314.htm#fromView=keyword&page=1&position=34&uuid=f95649b0-845f-42da-9ec3-37f985df8e8e&new_detail=true"
-          alt=""
-        />
-      </div>
+      <h1>Thank You for your response</h1>
+      <p>You can close this tab ...</p>{" "}
     </div>
   );
 };
