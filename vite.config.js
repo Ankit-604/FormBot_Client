9cbd83bcb61274da1ae7e187c47861e9f1d4e262
Ankit Kumar Sharma <kr704666@gmail.com>
2025-01-01 11:10:03 +0530

    updating

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 4173,
-    host: true,
+    port: process.env.PORT || 4173, // Bind to the Render-provided port, fallback to 4173
+    host: true, // Expose the app to external IPs
   },
   build: {
-    outDir: "dist",
-  },
-  define: {
-    "process.env": process.env, // Ensure all environment variables are available in your code
+    outDir: "dist", // Ensure that build output goes into the 'dist' folder
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: process.env.PORT || 4173, // Bind to the Render-provided port, fallback to 4173
-//     host: true, // Expose the app to external IPs
-//   },
-//   build: {
-//     outDir: "dist", // Ensure that build output goes into the 'dist' folder
-//   },
-// });
