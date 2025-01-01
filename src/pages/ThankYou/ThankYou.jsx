c27c6544c2414b8c2d4a67fc2617726bb66fff8d
Ankit Kumar Sharma <kr704666@gmail.com>
2025-01-01 16:55:01 +0530

    updating

--- src/pages/ThankYou/ThankYou.jsx
+++ src/pages/ThankYou/ThankYou.jsx
@@ -4,8 +4,8 @@ const ThankYou = () => {
   return (
     <div className={styles.thankYouContainer}>
       <div className={styles.content}>
-        <h1>Thank You!</h1>
-        <p>
+        <h1 className={styles.heading}>Thank You!</h1>
+        <p className={styles.para}>
           Your response has been received. Thank you for taking the time to
           share your thoughts with us.
         </p>
